8c34bdff1b1eaa652eb0791311b48d8de38af0ff
Lynn <interfectus13@gmail.com>
2016-07-02 22:25:51 -0400

    menu

---
+++ mock-menu-data.ts
@@ -0,0 +1,10 @@
+class menuItem {
+    id: number;
+    name: string;
+}
+
+export var MENU: menuItem[] = [
+    {id: 0, name: "Bacon"},
+    {id: 1, name: "Fries"},
+    {id: 2, name: "Lobster"}
+]
